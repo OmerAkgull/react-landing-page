cdea8ca9aa9b777edf7c8e1c8fb4b0085d5d327e
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-01-01 12:29:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./style.scss"
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos  dsadsa dsads fdsdsf
+      </p>
+    </>
+  )
+}
+
+export default App

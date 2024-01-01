8ac43ed56094ee1ae4d692a25281609b39995db5
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-01-01 12:30:54 +0300

    Apply css reset

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <p className="read-the-docs">
-        Click on the Vite and React logos  dsadsa dsads fdsdsf
+        Click on the Vite and React logos  dsadsa dsads fdsdsf asddsa fdsa
       </p>
     </>
   )

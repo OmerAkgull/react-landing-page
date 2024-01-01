61b433647c5b13d1f96b6748343af803afb1e070
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-01-01 13:12:56 +0300

    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./style.scss"
-
+import Navbar from "./sections/navbar/navbar"
 
 function App() {
   return (
     <>
       <p className="fs-3">
-        Click on the Vite and React logos dsadsa dsads fdsdsf asddsa fdsa fddsffds dsadas zaa 
+        Click on the Vite and React logos dsadsa dsads fdsdsf asddsa fdsa fddsffds dsadas zaa  dsadsa dsads
       </p>
+      <Navbar/>
     </>
   )
 }
--- src/sections/navbar/navbar.jsx
+++ src/sections/navbar/navbar.jsx
@@ -1,19 +1,20 @@
 import "./navbar.module.scss";
+import * as bootstrap from 'bootstrap'
 
-export function Navbar() {
+
+export default function Navbar() {
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div className="container">
+    <a className="navbar-brand" href="#">Start Bootstrap</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
         <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        <a className="nav-link" href="#">About</a>
+        <a className="nav-link" href="#">Contact</a>
       </div>
     </div>
   </div>
